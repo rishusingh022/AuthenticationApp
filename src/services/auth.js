22ba37a1cb0e446de5ddfc9c29f917493e81d7fb
rishusingh022 <rishusingh022@gmail.com>
2023-02-09 02:44:21 +0530

    feat : add redis to store token and collection import from postman update

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,6 +2,7 @@ const db = require('../../database/models');
 const { HTTPError } = require('../utils/errorHandler');
 const {encryptPassword} = require('../utils/encryptPassword');
 const jwt = require('jsonwebtoken');
+const {getRediwClient} = require('../utils/redis');
 
 const register = async (user) => {
   try {
@@ -21,6 +22,7 @@ const register = async (user) => {
 };
 const login = async (username, password) => {
   try {
+    const redisClient = await getRediwClient();
     const userRes = await db.Users.findOne({
       where: {
         username : username,
@@ -34,12 +36,23 @@ const login = async (username, password) => {
       throw new HTTPError('Invalid password', 401);
     }
     const token = jwt.sign(userRes.dataValues,'secret');
+    
+    await redisClient.set(token , '1');
+
     return {...userRes.dataValues,token};
   } catch (error) {
     throw new HTTPError(error.message, 500);
   }
 };
 const validateJwt = async (token) => {
+  const redisClient = await getRediwClient();
+  const userName = await redisClient.get(token);
+  console.log({userName});
+  if(!userName){
+    return {
+      message : 'Invalid token',
+    };
+  }
   const decodedToken = jwt.verify(token, 'secret');
   const userRes = await db.Users.findOne({
     where: {
